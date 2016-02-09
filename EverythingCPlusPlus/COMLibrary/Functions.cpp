0fc1c043e5894f926f06b9dc93f3bcb60b931abb
jw56578 <jw56578@gmail.com>
2016-02-09 12:10:20 -0600

    got C# working

--- EverythingCPlusPlus/COMLibrary/Functions.cpp
+++ EverythingCPlusPlus/COMLibrary/Functions.cpp
@@ -5,7 +5,7 @@
 if you look at COMExample for the same thing FunctionsExample.h
 you will see that there is a different calling convention, this is what i got from the pluralsight video
 but this does not work to export dlls to be consumed by C# managed code
-I was not able to find entry point DoSomething
+It was not able to find entry point DoSomething
 I had to add the extern "C"
 and i had to add the __declspec(dllexpoert) 
 and not use the __stdcall
